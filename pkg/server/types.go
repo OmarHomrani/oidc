d72b11a7a68f918bc025dad7b17d5d4aa82167b4
Homrani Omar <homrani.omar@gmail.com>
2024-12-30 13:06:30 +0100

    update main go

--- pkg/server/types.go
+++ pkg/server/types.go
@@ -1,5 +1,11 @@
 package server
 
+import (
+	"time"
+
+	"github.com/OmarHomrani/oidc/pkg/users"
+)
+
 type Config struct {
 	Apps      map[string]AppConfig `yaml:"apps"`
 	Url       string               `yaml:"url"`
@@ -7,7 +13,18 @@ type Config struct {
 }
 type AppConfig struct {
 	ClientID     string   `yaml:"clientID"`
-	ClientSecret string   `yaml:"clinetSecret"`
+	ClientSecret string   `yaml:"clientSecret"`
 	Issuer       string   `yaml:"issuer"`
 	RedirectURIs []string `yaml:"redirectURIs"`
+}
+
+type LoginRequest struct {
+	ClientID     string
+	RedirectURI  string
+	Scope        string
+	ResponseType string
+	State        string
+	CodeIssuedAt time.Time
+	User         users.User
+	AppConfig    AppConfig
 }
